09169bf34ec8192680506b92418025206618af30
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-01-12 23:30:39 +0900

    feat(RogueCharacter): spring arm and camera to character

--- Source/ActionRoguelike/RogueCharacter.cpp
+++ Source/ActionRoguelike/RogueCharacter.cpp
@@ -1,30 +1,31 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "RogueCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 
-#include "RogueCharacter.h"
-
-
-// Sets default values
 ARogueCharacter::ARogueCharacter()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	
 	PrimaryActorTick.bCanEverTick = true;
+	
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
 }
 
-// Called when the game starts or when spawned
 void ARogueCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void ARogueCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
 
-// Called to bind functionality to input
 void ARogueCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
