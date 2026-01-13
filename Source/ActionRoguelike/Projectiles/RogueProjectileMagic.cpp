09bed3de60e476c59d3adf8f28acd5ba41718a50
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-01-13 22:36:29 +0900

    feat(RogueProjectileMagic): create default magic projectile

---
+++ Source/ActionRoguelike/Projectiles/RogueProjectileMagic.cpp
@@ -0,0 +1,24 @@
+﻿#include "RogueProjectileMagic.h"
+
+#include "NiagaraComponent.h"
+#include "Animation/AnimInstanceProxy.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+ARogueProjectileMagic::ARogueProjectileMagic()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	RootComponent = SphereComponent;
+	SphereComponent->SetSphereRadius(16.f);
+	SphereComponent->SetCollisionProfileName(TEXT("Projectile"));
+
+	LoopedNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagaraComp"));
+	LoopedNiagaraComponent->SetupAttachment(SphereComponent);
+	
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMoveComp");
+	ProjectileMovementComponent->InitialSpeed = 2000.f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+}
+
