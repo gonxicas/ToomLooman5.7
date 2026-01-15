e82400463db7f8358ea781565b0695fe9f8ed923
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-01-16 00:42:28 +0900

    feat(RogueExplosiveBarrel): create explosive barrel

---
+++ Source/ActionRoguelike/RogueExplosiveBarrel.cpp
@@ -0,0 +1,62 @@
+﻿#include "RogueExplosiveBarrel.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+ARogueExplosiveBarrel::ARogueExplosiveBarrel()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	RootComponent = MeshComponent;
+	MeshComponent->SetSimulatePhysics(true);
+
+	LoopedAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("LoopedAudioComp"));
+	LoopedAudioComponent->SetAutoActivate(false);
+	LoopedAudioComponent->SetupAttachment(MeshComponent);
+
+	LoopedNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagaraComp"));
+	LoopedNiagaraComponent->SetAutoActivate(false);
+	LoopedNiagaraComponent->SetupAttachment(MeshComponent);
+	
+	RadialForceComponent = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComp"));
+	RadialForceComponent->SetupAttachment(MeshComponent);
+	RadialForceComponent->Radius = 1000.f;
+	RadialForceComponent->ImpulseStrength = 2000.f;
+}
+
+float ARogueExplosiveBarrel::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent,
+                                        class AController* EventInstigator, AActor* DamageCauser)
+{
+	LoopedAudioComponent->Activate();
+	LoopedAudioComponent->Play();
+	LoopedNiagaraComponent->Activate();
+
+	FTimerHandle TimerHandle;
+	GetWorldTimerManager().SetTimer(TimerHandle, this, &ARogueExplosiveBarrel::DestroyBarrel, DestroyDelayTime);
+	SetCanBeDamaged(false);
+	return Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+}
+
+void ARogueExplosiveBarrel::DestroyBarrel()
+{
+	LoopedAudioComponent->Stop();
+	LoopedAudioComponent->Deactivate();
+	LoopedNiagaraComponent->Deactivate();
+	
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ExplosionEffect, GetActorLocation()
+	                                               , FRotator::ZeroRotator);
+
+	UGameplayStatics::PlaySoundAtLocation(this, ExplosionSound, GetActorLocation(),
+	                                      FRotator::ZeroRotator);
+	
+	RadialForceComponent->FireImpulse();
+	Destroy();
+}
+
+void ARogueExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/ActionRoguelike/RogueExplosiveBarrel.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueExplosiveBarrel.generated.h"
+
+class URadialForceComponent;
+class UNiagaraSystem;
+class UNiagaraComponent;
+class UStaticMeshComponent;
+class USoundBase;
+class UAudioComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ARogueExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ARogueExplosiveBarrel();
+
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	TObjectPtr<UNiagaraSystem> ExplosionEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Sound")
+	TObjectPtr<USoundBase> ExplosionSound;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UNiagaraComponent> LoopedNiagaraComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UAudioComponent> LoopedAudioComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<URadialForceComponent> RadialForceComponent;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Explosion")
+	float DestroyDelayTime = 1.0f;
+	
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent,
+	                        class AController* EventInstigator, AActor* DamageCauser) override;
+	
+	virtual void DestroyBarrel();
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
