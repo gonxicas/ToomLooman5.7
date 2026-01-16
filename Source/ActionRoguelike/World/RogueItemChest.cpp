59eef5481db52ac7433fbaae417afee9a91cd02c
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-01-17 00:10:42 +0900

    feat(RogueItemChest): add chest animation

--- Source/ActionRoguelike/World/RogueItemChest.cpp
+++ Source/ActionRoguelike/World/RogueItemChest.cpp
@@ -1,26 +1,35 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "RogueItemChest.h"
 
-
-#include "RogueItemChest.h"
-
-
-// Sets default values
 ARogueItemChest::ARogueItemChest()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.SetTickFunctionEnable(false);
+
+	BaseMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMeshComp"));
+	RootComponent = BaseMeshComponent;
+	
+	LidMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LidMeshComp"));
+	LidMeshComponent->SetupAttachment(BaseMeshComponent);
 }
 
-// Called when the game starts or when spawned
 void ARogueItemChest::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	SetActorTickEnabled(true);
 }
 
-// Called every frame
 void ARogueItemChest::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	CurrentAnimationPitch = FMath::FInterpConstantTo(CurrentAnimationPitch, AnimationTargetPitch, DeltaTime, AnimationSpeed);
+	
+	LidMeshComponent->SetRelativeRotation(FRotator(CurrentAnimationPitch, 0.f, 0.f));
+	
+	if (FMath::IsNearlyEqual(CurrentAnimationPitch, AnimationTargetPitch))
+	{
+		SetActorTickEnabled(false);
+	}
 }
 
--- Source/ActionRoguelike/World/RogueItemChest.h
+++ Source/ActionRoguelike/World/RogueItemChest.h
@@ -4,17 +4,34 @@
 #include "GameFramework/Actor.h"
 #include "RogueItemChest.generated.h"
 
+class UStaticMeshComponent;
 UCLASS()
 class ACTIONROGUELIKE_API ARogueItemChest : public AActor
 {
 	GENERATED_BODY()
 
-public:
-	ARogueItemChest();
 
 protected:
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> BaseMeshComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> LidMeshComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	float CurrentAnimationPitch = 0.0f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	float AnimationSpeed = 50.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	float AnimationTargetPitch = 120.f;
+	
 	virtual void BeginPlay() override;
 
 public:
 	virtual void Tick(float DeltaTime) override;
+
+	ARogueItemChest();
 };
