ee30c0675fd3d50499118feba6900048f4881267
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-01-25 21:17:04 +0900

    feat(RoguePickUp): add health potion

---
+++ Source/ActionRoguelike/World/RoguePickUp.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RoguePickUp.generated.h"
+
+class USphereComponent;
+
+UCLASS(Abstract)
+class ACTIONROGUELIKE_API ARoguePickUp : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<USphereComponent> SphereComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Sound")
+	TObjectPtr<USoundBase> PickUpSound;
+	
+
+
+public:
+	virtual void PostInitializeComponents() override;
+	virtual void HandlePickUp();
+	UFUNCTION()
+	virtual void OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	                    int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	ARoguePickUp();
+};
---
+++ Source/ActionRoguelike/World/RoguePickUpHeal.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RoguePickUpHeal.h"
+
+#include "ActionSystem/RogueActionSystemComponent.h"
+
+
+ARoguePickUpHeal::ARoguePickUpHeal()
+{
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComponent->SetCollisionProfileName("NoCollision");
+	MeshComponent->SetupAttachment(RootComponent);
+}
+
+void ARoguePickUpHeal::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                      UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                      const FHitResult& SweepResult)
+{
+	const auto ActionSystemComponent =
+		OtherActor->GetComponentByClass<URogueActionSystemComponent>();
+	if (ensure(ActionSystemComponent == nullptr) || ActionSystemComponent->IsMaxHealth()) return;
+
+	ActionSystemComponent->ApplyHealthChange(HealAmount);
+	Super::OnActorOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
