9e76c1ab0baea6dd073448d0dcab55bfa20f88ed
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-02-01 21:09:55 +0900

    feat(RogueBTService_CheckRangeTo): check for range and line of sight

---
+++ Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.cpp
@@ -0,0 +1,30 @@
+﻿#include "RogueBTService_CheckRangeTo.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+
+void URogueBTService_CheckRangeTo::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	UBlackboardComponent* BBComp =  OwnerComp.GetBlackboardComponent();
+	check(BBComp);
+
+	const auto TargetActor = Cast<AActor>( BBComp->GetValueAsObject(TargetActorKey.SelectedKeyName));
+	if (!TargetActor) return;
+	
+	auto Controller = OwnerComp.GetAIOwner();
+	check(Controller);
+
+	auto bHasLOS = Controller->LineOfSightTo(TargetActor);
+
+	const auto OwningPawn = Controller->GetPawn();
+	check(OwningPawn);
+
+	const auto DistanceTo = FVector::Dist(TargetActor->GetActorLocation(), OwningPawn->GetActorLocation());
+
+	const bool bWithinRange = DistanceTo <= MaxAttackRange;
+	
+	BBComp->SetValueAsBool(WithinRangeKey.SelectedKeyName, bWithinRange && bHasLOS);
+}
---
+++ Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "RogueBTService_CheckRangeTo.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API URogueBTService_CheckRangeTo : public UBTService
+{
+	GENERATED_BODY()
+
+	UPROPERTY(Editanywhere, Category = "AI")
+	FBlackboardKeySelector TargetActorKey;
+	
+	UPROPERTY(Editanywhere, Category = "AI")
+	FBlackboardKeySelector WithinRangeKey;
+	
+	UPROPERTY(Editanywhere, Category = "AI")
+	float MaxAttackRange = 500;
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	
+};
